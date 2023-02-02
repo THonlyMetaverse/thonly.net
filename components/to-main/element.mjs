4fee695e86af24fe041f46e82c9a596b9cf6c670
Thon Ly <thonly@heartbank.ceo>
2023-02-02 10:23:45 -0800

    refactor

--- components/to-main/element.mjs
+++ components/to-main/element.mjs
@@ -14,8 +14,8 @@ class ToMain extends HTMLElement {
     async connectedCallback() {
         await import(`${THONLY}/components/tl-main/tl-kiitos/element.mjs`);
         await import(`${THONLY}/components/tl-main/tl-robot/element.mjs`);
-        this.#animate();
         this.#render(true);
+        this.#animate();
     }
 
     #render(refresh) {
